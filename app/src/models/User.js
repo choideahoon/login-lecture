271673a23e71b08b5ae888b9765ac2bc0cb414aa
cdh <cheese950308@gmail.com>
2022-04-01 00:23:53 +0900

    fs모듈을 통한 로그인 기능 구현

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -7,9 +7,9 @@ class User {
         this.body = body;
     
 }
-    login() {
-        const client = this.body;
-        const { id, passwd } = UserStorage.getUserInfo(client.id);
+    async login() {
+       const client = this.body;
+       const { id, passwd } = await UserStorage.getUserInfo(client.id);
         
     if (id){
         if (id === client.id && passwd === client.passwd) {
--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,19 +1,22 @@
 "use strict";
+const fs = require("fs").promises;
 
 class UserStorage {
 
-
-    static #users = {
-    id: ["cdh", "cd"],
-    passwd: ["123", "12"],
-    name: ["최대훈"],
-
-
-};
-
-
+    static #getUserInfo(data, id){
+        const users = JSON.parse(data);
+        const idx = users.id.indexOf(id);
+        const usersKeys = Object.keys(users);
+        const userInfo = usersKeys.reduce((newUser, info) => {
+            newUser[info] = users[info][idx];
+            return newUser;
+        }, {});
+      
+        return userInfo;
+    }
+    
     static getUsers(...fields){
-        const users = this.#users
+        //const users = this.#users
         const newUsers = fields.reduce((newUsers, field) => {                       
           if (users.hasOwnProperty(field)) {
               newUsers[field] = users[field];
@@ -23,17 +26,18 @@ class UserStorage {
         return newUsers;    
     }
     static getUserInfo(id) {
-            const users = this.#users
-            const idx = users.id.indexOf(id);
-            const usersKeys = Object.keys(users);
-            const userInfo = usersKeys.reduce((newUser, info) => {
-                newUser[info] = users[info][idx];
-                return newUser;
-            }, {})
-            return userInfo;
-         }
+        return fs.readFile("./src/databases/users.json")
+        .then((data) => {
+                return this.#getUserInfo(data, id);
+        })
+        .catch(console.error);
+
+    }
+
+
+
     static save(userInfo) {
-        const users = this.#users;
+        // const users = this.#users;
         users.id.push(userInfo.id);
         users.name.push(userInfo.name);
         users.passwd.push(userInfo.passwd);
